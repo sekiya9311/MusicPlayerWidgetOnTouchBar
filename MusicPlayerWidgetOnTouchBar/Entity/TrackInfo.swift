0be1a6ab1c21a6a6dcae531f30aace6fb2bb0da4
sekiya9311 <sekiya9311@gmail.com>
2019-08-02 00:40:34 +0900

    Add entity class of TrackInfo

---
+++ MusicPlayerWidgetOnTouchBar/Entity/TrackInfo.swift
@@ -0,0 +1,16 @@
+//
+//  TrackInfo.swift
+//  MusicPlayerWidgetOnTouchBar
+//
+//  Created by Ryota Sekiya on 2019/08/02.
+//  Copyright © 2019 sekiya9311. All rights reserved.
+//
+
+import Foundation
+
+struct TrackInfo: Codable {
+    let name: String
+    let artist: String
+    let album: String
+    let artworkUrl: String
+}
